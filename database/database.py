eb9c35cc2e2f7da71092e0726e14d5b7612a35dd
pietro <pietrolejobless@gmail.com>
2025-05-20 20:28:44 +0200

    dont mind

---
+++ database/database.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+
+
+class Database :
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.connection = None
+        self.cursor = None
+        self.connect()
+        self.create_tables()
+    
+    def connect(self):
+        pass
+        
+    def getSpellLis(self):
+        pass
+
+    
--- src/main.py
+++ src/main.py
@@ -20,4 +20,4 @@ def main():
     sys.exit(app.exec())
 
 if __name__ == '__main__':
-    main()
+    main(),
