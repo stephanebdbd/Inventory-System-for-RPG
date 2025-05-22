77f1e3aea56da2bd53e04d9a9a5c15b52e7483c4
pietro <pietrolejobless@gmail.com>
2025-05-22 16:15:03 +0200

    alongé sur l'canapé, chuis dans la maison hantée

--- src/database/importData.py
+++ src/database/importData.py
@@ -1,2 +1,31 @@
 import csv
 import xml
+import sys
+
+def importXML(file: str):
+    print(file)
+    pass
+
+def importCSV(file: str):
+    print(file)
+    pass
+
+def importJSON(file: str):
+    print(file)
+    pass
+
+def main():
+    while True:
+        sys.clear()
+        print("** Enter [q] to leave **\n")
+        file = input("Enter file name: ")
+        if "csv" in file:
+            importCSV(file)
+        if "xlm" in file:
+            importXML(file)
+        if "json" in file:
+            importJSON(file)
+
+
+if __name__ == "main":
+    main()
