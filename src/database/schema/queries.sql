dd972ff5f60b90c3ed79fd69d475d5ab2e368b08
pietro <pietrolejobless@gmail.com>
2025-05-23 15:16:11 +0200

    heheheheheh jahajhajhajha jijijijhiihihi

--- src/database/schema/queries.sql
+++ src/database/schema/queries.sql
@@ -1,10 +1,10 @@
--- 1. Les 10 joueurs ayant le plus d’or
+--TOPGOLD
 SELECT username, montant_or
 FROM Joueur
 ORDER BY montant_or DESC
 LIMIT 10;
 
--- 2. Le joueur ayant le plus de personnages de la même classe
+--RANK1
 SELECT j.username, p.classe, COUNT(*) AS total
 FROM Joueur j
 JOIN Personnage p ON j.id = p.joueur_id
@@ -12,13 +12,13 @@ GROUP BY j.username, p.classe
 ORDER BY total DESC
 LIMIT 1;
 
--- 3. La quête ayant la plus grosse récompense en or par niveau de difficulté
+--TOPQUEST
 SELECT nom_quete, niveau_difficulte, or_recompense
 FROM Quete
 ORDER BY (or_recompense / niveau_difficulte) DESC
 LIMIT 1;
 
--- 4. Le PNJ possédant l'inventaire avec la plus grande valeur en or cumulée
+--TOPPNJ
 SELECT p.nom, SUM(o.valeur_or) AS valeur_totale
 FROM PNJ p
 JOIN Inventaire i ON p.id = i.pnj_id
@@ -27,7 +27,7 @@ GROUP BY p.nom
 ORDER BY valeur_totale DESC
 LIMIT 1;
 
--- 5. Le type d'objet le plus souvent offert en récompense de quêtes de niveau 5
+--TOPITEM
 SELECT o.type_objet, COUNT(*) AS nombre
 FROM Recompense r
 JOIN Objet o ON r.objet_id = o.id
@@ -37,10 +37,11 @@ GROUP BY o.type_objet
 ORDER BY nombre DESC
 LIMIT 1;
 
--- 6. Les monstres avec les meilleures récompenses en or cumulées selon leurs PV
+--TOPMONSTER
 SELECT m.nom, SUM(o.valeur_or) AS total_or, m.vie
 FROM Monstre m
 JOIN Butin b ON m.id = b.monstre_id
 JOIN Objet o ON b.objet_id = o.id
 GROUP BY m.nom, m.vie
 ORDER BY total_or DESC;
+
