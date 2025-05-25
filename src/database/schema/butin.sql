d4eba46790f30b814e097d9cd1210497d09aecd0
meryla <meryemelauren@gmail.com>
2025-05-25 18:31:39 +0200

    test

--- src/database/schema/butin.sql
+++ src/database/schema/butin.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS Butin (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    monstre_id INT,
+   
     objet_id INT,
+     monstre_id INT,
     FOREIGN KEY (monstre_id) REFERENCES Monstre(id),
     FOREIGN KEY (objet_id) REFERENCES Objet(ObjetID)
 );
