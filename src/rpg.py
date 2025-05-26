26ee26b3a3f5dbb5a356a2b58983c8d41b2105a2
pietro <pietrolejobless@gmail.com>
2025-05-27 01:19:23 +0200

    mommyy

--- src/rpg.py
+++ src/rpg.py
@@ -1,7 +1,10 @@
 from controller.menuController import MenuController
 def main():
+    #db
+    #db.connect()
     menu = MenuController
     menu.launchView()
+    #db.discconnect()
     
 
 if __name__ == '__main__':
