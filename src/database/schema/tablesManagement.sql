a9fd7a38a89b18602ccdd3f0c241fff1930ecc32
Chris BADI BUDU <stephane.badi.budu@ulb.be>
2025-05-28 11:07:25 +0200

    well, I didn't have to edit the order. Sorry again

--- src/database/schema/tablesManagement.sql
+++ src/database/schema/tablesManagement.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS
+    PlayerSpell,
+    NPCQuest,
     ItemsDropped,
     MonsterLoot,
     Battle,
     Inventory,
-    NPC,
     Quests,
-    NPCQuest,
     Monster,
+    NPC,
     Player,
-    PlayerSpell,
     Characters,
     Spell,
     Item
