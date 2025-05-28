96f821d26d8e35c276e5a3ff766887936d24a3dc
Chris BADI BUDU <stephane.badi.budu@ulb.be>
2025-05-28 10:53:04 +0200

    tables finished

--- src/database/schema/inventory.sql
+++ src/database/schema/inventory.sql
@@ -1,21 +1,35 @@
 DROP TABLE IF EXISTS Inventory;
 
-CREATE TABLE IF NOT EXISTS Inventory ();
 -- parent class of inventories
+CREATE TABLE IF NOT EXISTS Inventory (
+    InventoryID INT AUTO_INCREMENT PRIMARY KEY UNIQUE,
+);
 
 CREATE TABLE IF NOT EXISTS CharacterInventory (
-    InventoryID INT AUTO_INCREMENT PRIMARY KEY,
-    CharID INT NOT NULL,
+    InventoryID INT NOT NULL,
+    CharID      INT NOT NULL,
     AmountSlots INT DEFAULT 0,
-    FOREIGN KEY (CharID) REFERENCES Characters(CharID),
+
+    PRIMARY KEY (CharID, InventoryID),
+    FOREIGN KEY (InventoryID)   REFERENCES Inventory(InventoryID),
+    FOREIGN KEY (CharID)        REFERENCES Characters(CharID),
 );
 
 CREATE TABLE IF NOT EXISTS NPCInventory (
-    InventoryID INT AUTO_INCREMENT PRIMARY KEY,
-    npcID INT NOT NULL,
-    FOREIGN KEY (npcID) REFERENCES NPC(npcID),
+    InventoryID INT NOT NULL,
+    npcID       INT NOT NULL,
+
+    PRIMARY KEY (InventoryID, npcID),
+    FOREIGN KEY (InventoryID)   REFERENCES Inventory(InventoryID),
+    FOREIGN KEY (npcID)         REFERENCES NPC(npcID),
 );
 
-CREATE TABLE IF EXISTS InventoryItem ()
--- linking tables between items and individual inventories by items counted by tuples
--- as characters can possess many of a same item
+CREATE TABLE IF NOT EXISTS InventoryItem (
+    InventoryID INT NOT NULL,
+    ItemID      INT NOT NULL,
+    AmountItem  INT NOT NULL DEFAULT 1,
+
+    PRIMARY KEY (InventoryID, ItemID),
+    FOREIGN KEY (InventoryID)   REFERENCES Inventory(InventoryID),
+    FOREIGN KEY (ItemID)        REFERENCES Item(ItemID),
+);
---
+++ src/database/schema/tablesManagement.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS
+    ItemsDropped,
+    MonsterLoot,
+    Battle,
+    Inventory,
+    Quests,
+    NPCQuest,
+    Monster,
+    NPC,
+    Player,
+    Characters,
+    Item
+CASCADE;
