58ca3da3e68f7bcbb39fcf4daf76ff74ec113d3b
pietro <pietrolejobless@gmail.com>
2025-05-30 13:01:24 +0200

    mini db script

---
+++ src/db_script.sh
@@ -0,0 +1,15 @@
+# faut faire cette comande avant pr pouvoir exec le script             chmod +x setup.sh
+# pr linux donc windows = fucked, mais c pas bien diff en sah
+
+DB_PASSWORD="yildizmygoat"
+DB_NAME="pietro"
+DB_USER="rpgg"
+
+echo " Setting up local database..."
+
+sudo mysql <<_EOF_
+CREATE DATABASE ${DB_NAME};
+CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';
+FLUSH PRIVILEGES;
+_EOF_
